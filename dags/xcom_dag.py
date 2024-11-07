a979974c3a74ce8bacfbc2ba321b50c968d7fdaf
Marcos Paricio <marcos.paricio@nexiona.com>
2024-11-07 12:18:24 +0100

    branch executing a task according to a condition and trigger rules

--- dags/xcom_dag.py
+++ dags/xcom_dag.py
@@ -1,11 +1,18 @@
 from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.operators.bash import BashOperator
+from airflow.utils.trigger_rule import TriggerRule
     
 from datetime import datetime
     
 def _t1(ti):
     ti.xcom_push(key='my_key', value=42)
+    
+def _branch(ti):
+    value = ti.xcom_pull(key='my_key', task_ids='t1')
+    if value == 42:
+        return 't2'
+    return 't3'
     
 def _t2(ti):
     print(ti.xcom_pull(key='my_key', task_ids='t1'))
@@ -23,6 +30,11 @@ with DAG('xcom_dag', default_args=default_args, schedule='@daily') as dag:
         python_callable=_t1
     )
     
+    branch = BranchPythonOperator(
+        task_id='branch',
+        python_callable=_branch
+    )
+    
     t2 = PythonOperator(
         task_id='t2',
         python_callable=_t2
@@ -33,4 +45,10 @@ with DAG('xcom_dag', default_args=default_args, schedule='@daily') as dag:
         bash_command="echo ''"
     )
     
-    t1 >> t2 >> t3
+    t4 = BashOperator(
+        task_id='t4',
+        bash_command="echo ''",
+        trigger_rule=TriggerRule.NONE_FAILED_MIN_ONE_SUCCESS
+    )
+    
+    t1 >> branch >> [t2, t3] >> t4
